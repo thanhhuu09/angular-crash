527eb41a4d2f7cfb39a98040ec3ab414e524dbef
Tommy Thach <thachtommy5@gmail.com>
2023-11-16 14:28:47 +0700

    Add Task interface and mock data.

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,25 @@
+import Task from './Task';
+// This is the mock data that we will use to populate our app.
+// TASKS is an array, so Task need [] around it.
+const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+];
+
+export default TASKS;
